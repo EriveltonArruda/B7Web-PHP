d0f67c50fe20b28516fb402f24df3c3fbc08d762
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2025-06-17 15:14:07 -0300

    Módulo 6: Orientação a Objetos - Aula: Oque é PSR

--- Módulo 6 - Orientação á Objetos/15_O que é PSR/index.php
+++ Módulo 6 - Orientação á Objetos/15_O que é PSR/index.php
@@ -1,1 +1,7 @@
 <?php
+/* O PHP de uns 10 anos pra cá teve uma fama ruim de ser desorganizado porque diferente do java que tem uma padronização de código, o PHP por si só possibilita você fazer a mesma coisa de infinitas formas diferentes, isso faz com que muitas gambiarras sejam feitas, a parte ruim é que você não tem padronização, cada programador cria do seu jeito, imagine um projeto open source ou até mesmo um projeto de uma empresa onde vários programadores fazem alterações do jeito que cada um quer, sem padronização alguma, fica muito difícil fazer uma manutenção no código dessa forma.
+
+Para melhorar isso, um grupo chamado PHP-FIG criou a PSR (PHP Standards Recommendations) ou seja, recomendações de padrão de PHP, note que não é uma obrigação, mas sim uma recomendação de padrão para ser usado no PHP.
+
+No próprio site deles tem uma lista de recomendações ([php-fig.org/psr/](https://www.php-fig.org/psr/)).
+*/
